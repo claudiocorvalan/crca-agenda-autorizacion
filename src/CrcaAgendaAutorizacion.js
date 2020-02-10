98e0fbd1b537fd385e9f40faab613b120e24c8c4
Claudio Corvalan <chiko6669@gmail.com>
2020-02-10 12:24:25 -0300

    se implementa solicitud de autorizacion

--- src/CrcaAgendaAutorizacion.js
+++ src/CrcaAgendaAutorizacion.js
@@ -1,4 +1,6 @@
 import { html, css, LitElement } from 'lit-element';
+import { axios} from 'axios';
+import qs from 'qs';
 
 import 'dile-modal';
 import 'dile-input';
@@ -7,8 +9,12 @@ export class CrcaAgendaAutorizacion extends LitElement {
   static get styles() {
     return css`
       :host {
-        --crca-agenda-autorizacion-text-color: #000;
-        color: var(--crca-agenda-autorizacion-text-color);
+        --dile-input-error-border-color: #dc3545;
+        --dile-input-label-color: var(--crca-agenda-autorizacion-text-color, #000);
+        color: var(--crca-agenda-autorizacion-text-color, #000);
+      }
+      div {
+        color: var(--dile-input-error-border-color);
       }
     `;
   }
@@ -16,9 +22,10 @@ export class CrcaAgendaAutorizacion extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      counter: { type: Number },
       urlAutorizacion: { type: String },
       urlAccion: { type: String },
+      dataId: { type: Number },
+      error: { type: String }
     };
   }
 
@@ -26,28 +33,60 @@ export class CrcaAgendaAutorizacion extends LitElement {
     super();
     this.title = '';
     this.urlAccion = '';
-    this.valor = '';
+    this.dataId = 0;
+    this.error = '';
   }
 
   render() {
     return html`
     <dile-modal id="modalAutorizacion" @dile-modal-closed="${this.__closed}">
       <h2>Autorización ${this.title}</h2>
-      <dile-input label="Firma Autorizador"></dile-input>
+      <dile-input 
+      id="input" 
+      label="Firma Autorizador" 
+      @enter-pressed=${this.__autorizar}
+      ?errored=${this.__hasError(this.error)}></dile-input>
+      ${this.__hasError(this.error) 
+        ? html`<div>${this.error}</div>` 
+        : ''}
       <button @click=${this.__autorizar}>Autorizar</button>
     </dile-modal>
     `;
   }
 
-  open(title, urlAccion){
+  open(title, urlAccion, dataId){
     this.title = title;
-    this.urlAccion=urlAccion;
+    this.urlAccion = urlAccion;
+    this.dataId = dataId;
     this.shadowRoot.getElementById('modalAutorizacion').open();
+  }
+
+  __autorizar() {
+    const data = { 'id': this.shadowRoot.getElementById('input').value };
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(data),
+      url: this.urlAutorizacion,
+    };
+    
+    axios(options)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   __closed() {
     this.title = '';
     this.urlAccion = '';
-    this.valor = '';
+    this.dataId = 0;
+    this.error = '';
+  }
+
+  __hasError(er) {
+    return er.lenght>0;
   }
 }
